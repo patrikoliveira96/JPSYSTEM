16cb9cc973d3d8dc6c2b59f178c26a4bedb7b378
patrikoliveira96 <patrik.oliveira@mycomp.com.br>
2020-06-16 13:07:00 -0300

    Atualizacao Menus e Barras Menu

--- src/view/Principal.java
+++ src/view/Principal.java
@@ -15,8 +15,24 @@ public class Principal {
     
     private JFrame janela;
     private JMenuBar barra;
-    private JMenu cadastro, vendas, compras, lavacao,cliente; 
-    private JMenuItem clientes,relatorio, relatorioCred, produtoforn, fornecedor, produtos,motorista,sacarias,camarafria,tabela,funcionarios,empresa,sair,teste;
+    //-------------------COMPONENTE DO MENU PRINCIPAL----------------//
+    private JMenu Mcadastro, Mvendas, Mcompras, Mlavacao,Mmaquinacao,Mfrete,Msacarias,Mcheque,Mbalanco,Mcontapagar,Mcaixa,Mfluxo,Mfinanceiro,Mestoque,Mviagem,Mconfiguracao,Mnfe,Msobre,Msair; 
+    
+    //----------------COMPONENTES DO SUB MENU COM MAIS COMPONENTES-------------//
+    private JMenu cliente,produtoforn, fornecedor, produtos,motorista,sacarias,camarafria,tabela,SCcadastro,CFestocagem;
+    private JMenuItem Cclientes,Crelatorio, CrelatorioCred, ProdForn, relatProdForn,funcionarios,empresa,sair;
+    
+    private JMenuItem Ffornecedor, Frelatorio, Fcategoria;
+    
+    private JMenuItem Pproduto,Prelatorio;
+    
+    private JMenuItem MTmotorista,MTrelatorio;
+    
+    private JMenuItem SCroduto,SCrelatorio, SCcliente,SCsacarias;
+    
+     private JMenuItem CFestoq,CFrelatorio;
+    
+    
     private JPanel painelIcon;
     
    
@@ -37,12 +53,26 @@ public class Principal {
         // ---------------CRIANDO O MENU --------------//
         
         
-        
-        
-        cadastro = new JMenu("Cadastros");       
-        vendas = new JMenu("Vendas");
-        compras = new JMenu("Compras");
-        lavacao = new JMenu("Lavação");
+        Mcadastro = new JMenu("Cadastros");       
+        Mvendas = new JMenu("Vendas");
+        Mcompras = new JMenu("Compras");
+        Mlavacao = new JMenu("Lavação");        
+        Mmaquinacao = new JMenu("Maquinação");       
+        Mfrete = new JMenu("Frete");
+        Msacarias = new JMenu("Sacarias");
+        Mcheque = new JMenu("Cheque");        
+        Mbalanco = new JMenu("Balanço");       
+        Mcontapagar = new JMenu("Contas a Pagar");
+        Mcaixa = new JMenu("Caixa");
+        Mfluxo = new JMenu("Fluxo");  
+        Mfinanceiro = new JMenu("Financeiro");       
+        Mestoque = new JMenu("Estoque");
+        Mviagem = new JMenu("Viagem");
+        Mconfiguracao = new JMenu("Configuração");
+        Mnfe = new JMenu("NF-e");       
+        Msobre = new JMenu("Sobre");
+        Msair = new JMenu("Sair");
+       
         
         //----------- CRIANDO PAINEL ICONES -----------------------//
         
@@ -56,32 +86,76 @@ public class Principal {
             
         // ---------------CRIANDO O SUB MENU--------------//
          cliente = new JMenu("Cliente");      
-         clientes = new JMenuItem("Clientes");
-         relatorio = new JMenuItem("Relatório");
-         relatorioCred= new JMenuItem("Relatório Limite de Crédito");
+         Cclientes = new JMenuItem("Clientes");
+         Crelatorio = new JMenuItem("Relatório");
+         CrelatorioCred= new JMenuItem("Relatório Limite de Crédito");
          
-         cliente.add(clientes);
-         cliente.add(relatorio);
-         cliente.add(relatorioCred);
+         cliente.add(Cclientes);
+         cliente.add(Crelatorio);
+         cliente.add(CrelatorioCred);
          
           //----------CHAMANDO CADASTRO DE CLIENTE----------------//
-         clientes.addActionListener((java.awt.event.ActionEvent evt)->{
+         Cclientes.addActionListener((java.awt.event.ActionEvent evt)->{
              ConsultaCliente cadConsCliente = new ConsultaCliente();
              cadConsCliente.desenharTela(); 
              
          });
          
         
-         produtoforn = new JMenuItem("Produto/Fornecedor");
-         fornecedor = new JMenuItem("Fornecedor");
-         produtos = new JMenuItem("Produtos");
-         motorista = new JMenuItem("Motorista");
-         sacarias = new JMenuItem("Sacarias");
-         camarafria = new JMenuItem("Câmara Fria");
-         tabela = new JMenuItem("Tabelas");
+         produtoforn = new JMenu("Produto/Fornecedor");
+         ProdForn= new JMenuItem("Produto / Fornecedor");
+         relatProdForn = new JMenuItem("Relatório");
+         
+         produtoforn.add(ProdForn);
+         produtoforn.add(relatProdForn);
+         
+    
+        
+          fornecedor = new JMenu("Fornecedor");
+          Ffornecedor = new JMenuItem("Fornecedores");
+          Frelatorio = new JMenuItem("Relatório");
+          Fcategoria = new JMenuItem("Categoria/Ramo Atividade");
+          
+          fornecedor.add(Ffornecedor);
+          fornecedor.add(Frelatorio);
+          fornecedor.add(Fcategoria);
+          
+          
+         produtos = new JMenu("Produtos");
+         Pproduto = new JMenuItem("Produtos");
+         Prelatorio = new JMenuItem("Relatório");
+         
+         produtos.add(Pproduto);
+         produtos.add(Prelatorio);
+         
+         motorista = new JMenu("Motorista");
+         MTmotorista = new JMenuItem("Motorista");
+         MTrelatorio = new JMenuItem("Relatório");
+         
+         motorista.add(MTmotorista);
+         motorista.add(MTrelatorio);
+         
+         sacarias = new JMenu("Sacarias");      
+         SCcadastro = new JMenu("Cadastro");   
+         SCcliente= new JMenuItem("Cliente");
+         SCsacarias = new JMenuItem("Sacarias");
+         SCrelatorio = new JMenuItem("Relatório de Estoque");
          
          
-         funcionarios = new JMenuItem("Funcionários");         
+         
+         sacarias.add(SCcadastro);
+         sacarias.add(SCrelatorio);
+         SCcadastro.add(SCcliente);
+         SCcadastro.add(SCsacarias);
+         
+         camarafria = new JMenu("Câmara Fria");
+         CFestocagem = new JMenu("Estocagem");
+         
+         tabela = new JMenu("Tabelas");
+         
+         
+         funcionarios = new JMenuItem("Funcionários");  
+         
           //----------CHAMANDO CADASTRO DE FUNCIONARIO----------------//
          funcionarios.addActionListener((java.awt.event.ActionEvent evt)->{
              CadastroFuncionario cadFuncionario = new CadastroFuncionario();
@@ -90,6 +164,7 @@ public class Principal {
          
          
          empresa = new JMenuItem("Empresa"); 
+         
          //----------CHAMANDO CADASTRO DE EMPRESA----------------//
          empresa.addActionListener((java.awt.event.ActionEvent evt)->{
              CadastroEmpresa cadEmpresa = new CadastroEmpresa();
@@ -97,29 +172,44 @@ public class Principal {
          });
          sair = new JMenuItem("Sair");
          
-         
-        barra.add(cadastro);
-        barra.add(vendas);
-        barra.add(compras);
-        barra.add(lavacao);
-     
-        teste = new JMenuItem("te11ste");
+        //------ MENU BARRA ----------------// 
+        barra.add(Mcadastro);
+        barra.add(Mvendas);
+        barra.add(Mcompras);
+        barra.add(Mlavacao);
+        barra.add(Mmaquinacao);
+        barra.add(Mfrete);
+        barra.add(Msacarias);
+        barra.add(Mcheque); 
+        barra.add(Mbalanco);
+        barra.add(Mcontapagar);
+        barra.add(Mcaixa);
+        barra.add(Mfluxo);
+        barra.add(Mfinanceiro);
+        barra.add(Mestoque);
+        barra.add(Mviagem);
+        barra.add(Mconfiguracao);
+        barra.add(Mnfe);
+        barra.add(Msobre);
+        barra.add(Msair);
+        
+        
         
         
         
         //------------------- ADICIONANDO OS COMPONENTES AO SUB MENU  ------------------//
         
-        cadastro.add(cliente);        
-        cadastro.add(produtoforn);
-        cadastro.add(fornecedor);
-        cadastro.add(produtos);
-        cadastro.add(motorista);
-        cadastro.add(sacarias);
-        cadastro.add(camarafria);
-        cadastro.add(tabela);
-        cadastro.add(funcionarios);
-        cadastro.add(empresa);
-        cadastro.add(sair);
+        Mcadastro.add(cliente);        
+        Mcadastro.add(produtoforn);
+        Mcadastro.add(fornecedor);
+        Mcadastro.add(produtos);
+        Mcadastro.add(motorista);
+        Mcadastro.add(sacarias);
+        Mcadastro.add(camarafria);
+        Mcadastro.add(tabela);
+        Mcadastro.add(funcionarios);
+        Mcadastro.add(empresa);
+        Mcadastro.add(sair);
         
         
         janela.setJMenuBar(  barra );
